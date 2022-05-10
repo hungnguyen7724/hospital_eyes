6f6d52d0ef5f5a4af7352b648b09af8defbeb40d
xuanhung3715 <19211tt3715@mail.tdc.edu.vn>
2022-05-10 22:07:42 +0700

    update admin

---
+++ public/js/script.js
@@ -0,0 +1,10 @@
+// $(window).on('loader', function(event){
+//     $('body').removeClass('preloading');
+//     $('loader').delay(100).fadeOut('fast');
+// });
+
+$(window).on('load', function(event) {
+    $('body').removeClass('preloading');
+    // $('.load').delay(1000).fadeOut('fast');
+    $('.loader').delay(1000).fadeOut('fast');
+});
